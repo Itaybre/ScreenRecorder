20e39d49aaab0f69a283785fa87389f79d3e7648
Itay <itay@itaysoft.com>
2020-07-09 11:58:35 -0300

    Initial Project

---
+++ ScreenRecorder/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScreenRecorder
+//
+//  Created by Itay Brenner on 7/9/20.
+//  Copyright © 2020 Itaysoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
